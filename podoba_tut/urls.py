a027c74cc208e91dc1c74a4c069b2dd540252583
Posseydon <Posseydon87@gmail.com>
2018-02-07 09:43:57 +0200

    add dinamic students_list

--- podoba_tut/urls.py
+++ podoba_tut/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+
+from students import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Students urls
+	path('', views.students_list, name='home'),
+
+	path('students/add/', views.students_add, name='students_add'),
+
+	path('students/<sid>/edit/', views.students_edit, name='students_edit'),
+
+	path('students/<sid>/delete/', views.students_delete, name='students_delete'),
+
+	# Groups urls
+	path('groups/', views.groups_list, name='groups'),
+
+	path('groups/add/', views.groups_add, name='groups_add'),
+
+	path('groups/<gid>/edit/', views.groups_edit, name='groups_edit'),
+	
+	path('groups/<gid>/delete/', views.groups_delete, name='groups_delete'),
+
+
+
+	# re_path('^admin/', include(admin.site.urls)),
+
 ]
